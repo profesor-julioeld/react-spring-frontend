6a56100b038dac96397457afd396473fc4556172
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2023-02-21 23:47:46 +0100

    Vuelvo a quitar process.env.NODE_END

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       };
   }, []);
 
-  
+  // Cambio Aprende React en {process.env.NODE_ENV}
   return (
     <div className="App">
       <header className="App-header">
@@ -37,7 +37,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React en {process.env.NODE_ENV}
+           Aprende React
         </a>
       </header>
     </div>
